8d64dc312f66fd4e1ffb7af5cd573155b97f89f9
PJ <byinarie@gmail.com>
2023-04-14 10:37:51 -0500

    Update ShodanTool.py
    Fixed CSV output not working 
    
    Added --api-key argument

--- ShodanTool.py
+++ ShodanTool.py
@@ -2,50 +2,68 @@ import shodan
 import click
 from termcolor import colored
 import os
-#from tqdm import tqdm
 import csv
-#import time
 
 @click.command()
 @click.option('--orgname', '-on', prompt='Enter the name of the organization', help='The name of the organization.')
 @click.option('--output', '-op', default='shodan_output.csv', help='Name of the output file.')
+@click.option('--api-key', '-ak', required=True, help='Shodan API key.')
 
-def search(orgname, output):
-    # Replace with your actual Shodan API key
-    api = shodan.Shodan('API-KEY')
+def search(orgname, output, api_key):
+    # Initialize Shodan API with provided API key
+    api = shodan.Shodan(api_key)
+
     try:
-        # Search
+        # Search for devices related to the input organization
         results = api.search(f'org:"{orgname}"')
-        file_name = output
 
         # Print the number of devices found
         print(colored(f'Results found: {results["total"]}', 'green'))
 
-        # Iterate through the results and print vulnerabilities
+        # Create a dictionary to store vulnerabilities by host
+        vuln_dict = {}
+
+        # Iterate through the results and store vulnerabilities by host
         for result in results['matches']:
-            print(colored(f'Device: {result["ip_str"]}:{result["port"]}', 'yellow'))
+            ip_str = result["ip_str"]
+            port = result["port"]
             if 'vulns' in result:
                 for vuln in result['vulns']:
-                    print(colored(f'\tVulnerability: {vuln}', 'red'))
-            else:
-                print(colored(f'\tVulnerability: None Found', 'green'))
-        # Search Shodan for the orgs IP address(s)
+                    if ip_str in vuln_dict:
+                        vuln_dict[ip_str].append(vuln)
+                    else:
+                        vuln_dict[ip_str] = [vuln]
+
+        # Search Shodan for the org's IP address(es)
         results = api.search("org:{}".format(orgname))
 
-        # File Creation
-        if not os.path.exists(file_name):
-            open(file_name, "w").close()
+        # Output to CSV file
+        file_name = output
 
-        # Output
+        # Check if file already exists
+        if os.path.exists(file_name):
+            print(colored(f'WARNING: {file_name} already exists and will be overwritten.', 'yellow'))
+
+        # Write output to CSV file
         with open(file_name, "w", newline="") as csvfile:
-            fieldnames = ['IP', 'OS', 'Port', 'Hostnames']
+            fieldnames = ['IP', 'OS', 'Port', 'Hostnames', 'Vulnerabilities']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
 
-        # Write output to CSV
+            # Write header row if file is new
+            if not os.path.exists(file_name) or os.path.getsize(file_name) == 0:
+                writer.writeheader()
+
+            # Write data to file
             for result in results['matches']:
-                writer.writerow({'IP': result['ip_str'], 'OS': result.get('os', 'N/A'), 'Port': result['port'],
-                                    'Hostnames': result.get('hostnames', 'N/A')})
+                ip_str = result['ip_str']
+                row_dict = {'IP': ip_str, 'OS': result.get('os', 'N/A'), 'Port': result['port'],
+                            'Hostnames': result.get('hostnames', 'N/A')}
+                if ip_str in vuln_dict:
+                    row_dict['Vulnerabilities'] = vuln_dict[ip_str]
+                else:
+                    row_dict['Vulnerabilities'] = 'None Found'
+                writer.writerow(row_dict)
+
         print(f"Data exported successfully to {file_name}")
 
     except shodan.APIError as e:
